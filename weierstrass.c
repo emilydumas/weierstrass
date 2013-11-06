fb8f861dd37f4237b68d66af243f38d91329f3ad
David Dumas <david@dumas.io>
2013-11-06 15:04:24 -0500

    Remove unused theta function code

--- weierstrass.c
+++ weierstrass.c
@@ -2,7 +2,7 @@
  * Filename:      weierstrass.c
  * Description:   the Weierstrass P function
  * Author:        David Dumas <david@dumas.io>
- * Modified at:   Wed Nov  6 14:27:41 2013
+ * Modified at:   Wed Nov  6 15:04:02 2013
  *                
  * Copyright (C) 2013  David Dumas
  *                
@@ -13,8 +13,6 @@
 #include <gsl/gsl_math.h>
 #include <gsl/gsl_complex.h>
 #include <gsl/gsl_complex_math.h>
-
-#include <stdio.h>
 
 #define THETA_ITER_MAX 200
 
@@ -32,12 +30,6 @@
 
 /* 1/7 */
 #define _CONST_1_7 0.14285714285714285714285714285714285714285714285714
-
-/* For printf debugging (!) */
-void show(const char *name, gsl_complex value)
-{
-  printf("%s = %f + I %f\n",name,GSL_REAL(value),GSL_IMAG(value));
-}
 
 gsl_complex pow4(gsl_complex x)
 {
@@ -361,117 +353,4 @@ void wP_and_prime(gsl_complex z, gsl_complex tau, const gsl_complex *g, gsl_comp
     *pp = ppsolve;
   else
     *pp = gsl_complex_negative(ppsolve);
-}
-
-/* ---------------------------------------------------------------------- */
-/*                   UNUSED FUNCTIONS BELOW                               */
-/* ---------------------------------------------------------------------- */
-
-/*
-Both P(z,tau) and P'(z,tau) can be written in the form
-
-   a(tau) + b(tau) * M(z,tau)
-
-where a,b are complex constants depending only on the lattice, and
-where M is a laurent monomial in theta functions of pi*z.
-
-This function computes the constants a1,b1 (for P) and b2 (for P').
-Since a2=0 this constant is not included.
-*/
-
-void compute_lattice_coefs(gsl_complex tau, gsl_complex *a1, gsl_complex *b1, gsl_complex *b2)
-{
-  gsl_complex q, q14;
-  gsl_complex t2,t3,t4,t24,t34,tprod;
-  gsl_complex g2, g3;
-  gsl_complex g3_term1, g3_term2;
-  gsl_complex alpha, beta, gamma, Delta, sDelta;
-  gsl_complex e1;
-
-  q = gsl_complex_exp(gsl_complex_mul_imag(tau,M_PI));
-  q14 = gsl_complex_exp(gsl_complex_mul_imag(tau,M_PI_4));
- 
-  t2=theta20(q,q14);
-  t3=theta30(q);
-  t4=theta40(q);
-
-  t24 = pow4(t2);
-  t34 = pow4(t3);
-  tprod = gsl_complex_mul(t2,gsl_complex_mul(t3,t4));
-
-  g2 = gsl_complex_mul_real(gsl_complex_sub(gsl_complex_add(gsl_complex_mul(t24,t24),gsl_complex_mul(t34,t34)),gsl_complex_mul(t24,t34)),_CONST_43PI4);
-
-  g3_term1 = gsl_complex_add(gsl_complex_mul(t24,gsl_complex_mul(t24,t24)),gsl_complex_mul(t34,gsl_complex_mul(t34,t34)));
-  
-  g3_term2 = gsl_complex_mul(gsl_complex_add(t24,t34),gsl_complex_mul(t24,t34));
-
-  g3 = gsl_complex_sub( gsl_complex_mul_real(g3_term1, _CONST_827PI6),
-			gsl_complex_mul_real(g3_term2, _CONST_49PI6) );
-
-
-  Delta = gsl_complex_sub(gsl_complex_mul(g3,g3),gsl_complex_mul_real(gsl_complex_mul(g2,gsl_complex_mul(g2,g2)),1.0/27.0));
-
-  sDelta = gsl_complex_sqrt(Delta);
-  /* if (GSL_IMAG(sDelta) < 0.0) */
-  /*   sDelta = gsl_complex_negative(sDelta); */
-
-  alpha = gsl_complex_sqrt(gsl_complex_mul_real(g2,1.0/12.0));
-  /* if (GSL_IMAG(alpha) < 0.0) */
-  /*   alpha = gsl_complex_negative(alpha); */
-
-  beta = gsl_complex_div(gsl_complex_pow_real(gsl_complex_add(g3,sDelta),1.0/3.0),gsl_complex_mul_real(alpha,2.0));
-
-  gamma = gsl_complex_inverse(beta);
-
-  *a1 = gsl_complex_mul(alpha,gsl_complex_add(beta,gamma));
-  *b1 = gsl_complex_mul_real(gsl_complex_mul(gsl_complex_mul(t3,t3),gsl_complex_mul(t4,t4)),M_PI*M_PI);
-  *b2 = gsl_complex_mul_real(gsl_complex_mul(tprod,tprod),-2.0*M_PI*M_PI*M_PI);
-
-  /* Problems with selecting the wrong root of the Weierstrass polynomial.  Debug... */
-  show("tau",tau);
-  show("g2",g2);
-  show("g3",g3);
-  show("Delta",Delta);
-  show("sDelta",sDelta);
-  show("alpha",alpha);
-  show("beta",beta);
-  show("gamma",gamma);
-  show("a1",*a1);
-  show("b1",*b1);
-  show("b2",*b2);
-}
-
-/* Compute P using theta functions, where lattice coefficients a,b are known */
-gsl_complex wP_theta_lc(gsl_complex z, gsl_complex tau, gsl_complex a1, gsl_complex b1)
-{
-  gsl_complex q,q14;
-  gsl_complex t1,t2;
-
-  q = gsl_complex_exp(gsl_complex_mul_imag(tau,M_PI));
-  q14 = gsl_complex_exp(gsl_complex_mul_imag(tau,M_PI_4));
- 
-  t1 = theta1(gsl_complex_mul_real(z,M_PI),q,q14);
-  t2 = theta2(gsl_complex_mul_real(z,M_PI),q,q14);
-
-  return gsl_complex_add(a1,gsl_complex_mul(b1,gsl_complex_div(gsl_complex_mul(t2,t2),gsl_complex_mul(t1,t1))));
-}
-
-/* Compute P' using theta functions, where lattice coefficients a,b are known */
-gsl_complex wP_prime_theta_lc(gsl_complex z, gsl_complex tau, gsl_complex b2)
-{
-  gsl_complex q,q14;
-  gsl_complex t1,t2,t3,t4;
-  gsl_complex piz;
-
-  q = gsl_complex_exp(gsl_complex_mul_imag(tau,M_PI));
-  q14 = gsl_complex_exp(gsl_complex_mul_imag(tau,M_PI_4));
- 
-  piz = gsl_complex_mul_real(z,M_PI);
-
-  t1 = theta1(piz,q,q14);
-  t2 = theta2(piz,q,q14);
-  t3 = theta3(piz,q);
-  t4 = theta4(piz,q);
-
-  return gsl_complex_mul(b2,gsl_complex_div(gsl_complex_mul(t2,gsl_complex_mul(t3,t4)), gsl_complex_mul(t1,gsl_complex_mul(t1,t1))));
 }
